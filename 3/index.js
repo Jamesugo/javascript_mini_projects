074274f286d581f24e92027b3ed9127c36679ba0
city <jamesugochukwu194@gmail.com>
2024-07-15 02:18:51 +0100

    added projects

---
+++ 3/index.js
@@ -0,0 +1,14 @@
+const quotes = [
+    'The purpose of our life is to be happy'
+    'Get busy livng or get busy dying'
+    'The only way to do great work is to love what you do'
+    "Learn how to walk before you run"
+]
+
+
+const usedIndexes = new set()
+const quoteElement = document.getElementById('quote')
+
+function generateQuote() {
+    quoteElement.innerHTML = 'Hello world'
+}
